2da3b6244ef2faaef803f37de1df12f25890ce29
chavab1 <chavab_1@yahoo.com>
2017-02-03 15:40:27 -0800

    Add stylesheet and  stylesheet import function

--- folio.php
+++ folio.php
@@ -7,6 +7,7 @@
     $cssFile = "./media/com_folio/css/site.stylesheet.css";
     $document->addStyleSheet($cssFile);
 
+
     $controller = JControllerLegacy::getInstance('Folio');
     $controller->execute(JFactory::getApplication()->input->get('task'));
     $controller->redirect();
