4e67d6601cbdfd0f0b3830d1adea0ec18be61192
chavab1 <chavab_1@yahoo.com>
2017-02-03 15:10:54 -0800

    Add function to folio.php to load frontend stylesheet

--- folio.php
+++ folio.php
@@ -2,6 +2,11 @@
 
     defined('_JEXEC') or die;
 
+    // Add stylesheet to frontend
+    $document = JFactory::getDocument();
+    $cssFile = "./media/com_folio/css/site.stylesheet.css";
+    $document->addStyleSheet($cssFile);
+
     $controller = JControllerLegacy::getInstance('Folio');
     $controller->execute(JFactory::getApplication()->input->get('task'));
     $controller->redirect();
